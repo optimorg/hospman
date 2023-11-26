d09c2d7afa99c6d9ead3534ae5f6601198fb6293
morgREEN <morgreen25@gmail.com>
2023-11-26 17:22:37 -0500

    Moved material modules from app module to material module

--- src/app/material/material/material.module.ts
+++ src/app/material/material/material.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 @NgModule({
   declarations: [],
   imports: [
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   exports:[
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }
