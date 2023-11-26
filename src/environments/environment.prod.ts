b765e6dc05d4b420e3c4b45aae37505bee3b02dc
morgREEN <morgreen25@gmail.com>
2023-11-25 19:07:32 -0500

    Added fire and firestore module + initialzed app

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig : {
+        apiKey: "AIzaSyAMET-nb65OghAHhFJUWa_sMzNdK9rYY-I",
+        authDomain: "hospital-management-9b4e7.firebaseapp.com",
+        projectId: "hospital-management-9b4e7",
+        storageBucket: "hospital-management-9b4e7.appspot.com",
+        messagingSenderId: "220147173549",
+        appId: "1:220147173549:web:674cdbd205d82e5928de24"
+      }
+};
