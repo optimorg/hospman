5265eb9aa53afb357ba9a23be5f7edafe0a54c24
morgREEN <morgreen25@gmail.com>
2023-11-26 15:01:59 -0500

    Doctor and patient components and sidebar with working navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PatientComponent } from './component/dashboard/patient/patient.component';
+import { DoctorComponent } from './component/dashboard/doctor/doctor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'dashboard', children: [
+    {path : '', redirectTo: 'patient', pathMatch: 'full'},
+    {path: 'patient', component: PatientComponent},
+    {path: 'doctor', component: DoctorComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,33 @@ import { AppComponent } from './app.component';
 import {AngularFireModule} from '@angular/fire/compat';
 import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
 import {environment} from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material/material.module';
+import { DoctorComponent } from './component/dashboard/doctor/doctor.component';
+import { SidebarComponent } from './component/dashboard/sidebar/sidebar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
-    MaterialModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
 
   ],
   providers: [],
